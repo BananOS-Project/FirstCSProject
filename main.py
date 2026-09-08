b93db24c5b7ca293ea6b1517864a1da596224835
Banan <burkeowen12@gmail.com>
2026-09-08 12:07:10 -0400

    Added input to program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 message = "Hello Comp151"
 print(message)
-print("And Hello Comp143")
-print(message)
+your_name = input("What is your name? ")
+print("Hello,",your_name)
+print("Very nice to meet you, have a nice day!")
