2eb36953d67d9e14de2c5f2f796bec6a51f7ef88
Banan <burkeowen12@gmail.com>
2026-09-10 11:11:05 -0400

    Added input to program

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 message = "Hello Comp151"
 print(message)
 your_name = input("What is your name? ")
-print("Hello,",your_name)
-print("Very nice to meet you")
-your_major = input("What is your major? ")
+print("And Hello Comp151,",your_name)
+message = "Students"
+print(message)
